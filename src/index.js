b0451b34661fa57bcad31dc294764700b34fd379
TeriFash <teri.fash@gmail.com>
2020-08-12 11:48:07 +0300

    avtoformater

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import { SimplerFetch } from "./modules"
-import SimplerSearch from './modules/search';
-import "./style.scss"
+import { SimplerFetch } from "./modules";
+import SimplerSearch from "./modules/search";
+import "./style.scss";
+
+console.log("\x1b[44m 🧐 ➜", "");
 
 {
-    const dataHead = [
-        'XMLHttpRequest and Fetch in JavaScript',
-        'Open chrome dev-tools console'
-    ]
-    
-    dataHead.map((item, i) => {
-        const node = !i ? 'h1' : 'p'
-        
-        document.querySelector(`#app ${node}`).innerHTML = item
-    })
+  const dataHead = [
+    "XMLHttpRequest and Fetch in JavaScript",
+    "Open chrome dev-tools console",
+  ];
+
+  dataHead.map((item, i) => {
+    const node = !i ? "h1" : "p";
+
+    document.querySelector(`#app ${node}`).innerHTML = item;
+  });
 }
 
 const usersOpt = {
-    // element: 'span'
-}
-const users = new SimplerFetch('users', usersOpt)
+  // element: 'span'
+};
+const users = new SimplerFetch("users", usersOpt);
 
-users.getRespond()
-    
-const usersSearch = new SimplerSearch()
+users.getRespond();
 
-
+const usersSearch = new SimplerSearch();
--- src/modules/xhr.js
+++ src/modules/xhr.js
@@ -1,21 +1,29 @@
-export default function simplerXHR(url = '', method = 'GET', body = null, options = {}) {
+export default function simplerXHR(
+  url = "",
+  method = "GET",
+  body = null,
+  options = {}
+) {
   return new Promise((resolve, reject) => {
-    const finalUrl = options.urlCustom ? url : `https://jsonplaceholder.typicode.com/${url}`
-    const xhr = new XMLHttpRequest()
+    const finalUrl = options.urlCustom
+      ? url
+      : `https://jsonplaceholder.typicode.com/${url}`;
+    const xhr = new XMLHttpRequest();
 
-    xhr.open(method, finalUrl)
-    xhr.responseType = 'json'
-    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.open(method, finalUrl);
+    xhr.responseType = "json";
+    xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onload = () => {
-      if(xhr.status >= 400) {
+      if (xhr.status >= 400) {
         reject(xhr.response);
       } else {
         resolve(xhr.response);
       }
-    }
+    };
+
     xhr.onerror = () => {
       reject(xhr.response);
-    }
-    xhr.send(JSON.stringify(body))
-  })
+    };
+    xhr.send(JSON.stringify(body));
+  });
 }
